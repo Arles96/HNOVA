19db93796846b120274362c2b321398ca5e477d8
Arles29 <arles.cerrato@gmail.com>
2025-10-05 18:00:15 -0600

    fix: correccion csv file and send feedback correcly

--- hnova-front/lib/csv.tsx
+++ hnova-front/lib/csv.tsx
@@ -7,129 +7,127 @@ export const resultsCsv = (lines: string[], headers: string[]): IExoplanetData[]
       (obj: IExoplanetData, header, index) => {
         const name = header.trim()
         if (ExoplanetFieldEnum.hostId === name) {
-          obj.hostId = values[index]?.trim() || undefined
-        } else if (ExoplanetFieldEnum.isExoplanet === name) {
-          obj.isExoplanet = values[index]?.trim() ? values[index]?.trim().toLowerCase() === "true" : undefined
+          obj.hostId = values[index]?.trim() || ''
         } else if (ExoplanetFieldEnum.hostName === name) {
-          obj.hostName = values[index]?.trim() || undefined
+          obj.hostName = values[index]?.trim() || ''
         } else if (ExoplanetFieldEnum.k2Id === name) {
-          obj.k2Id = values[index]?.trim() || undefined
+          obj.k2Id = values[index]?.trim() || ''
         } else if (ExoplanetFieldEnum.kepoiName === name) {
-          obj.kepoiName = values[index]?.trim() || undefined
+          obj.kepoiName = values[index]?.trim() || ''
         } else if (ExoplanetFieldEnum.orbitalPeriod === name) {
-          obj.orbitalPeriod = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.orbitalPeriod = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.orbitalPeriodMin === name) {
-          obj.orbitalPeriodMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.orbitalPeriodMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.orbitalPeriodMax === name) {
-          obj.orbitalPeriodMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.orbitalPeriodMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.transitDuration === name) {
-          obj.transitDuration = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.transitDuration = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.transitDurationMin === name) {
-          obj.transitDurationMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.transitDurationMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.transitDurationMax === name) {
-          obj.transitDurationMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.transitDurationMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.transitDepth === name) {
-          obj.transitDepth = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.transitDepth = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.transitDepthMin === name) {
-          obj.transitDepthMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.transitDepthMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.transitDepthMax === name) {
-          obj.transitDepthMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.transitDepthMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.impactParameter === name) {
-          obj.impactParameter = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.impactParameter = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.impactParameterMin === name) {
-          obj.impactParameterMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.impactParameterMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.impactParameterMax === name) {
-          obj.impactParameterMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.impactParameterMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetRadiusEarth === name) {
-          obj.planetRadiusEarth = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetRadiusEarth = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetRadiusEarthMin === name) {
-          obj.planetRadiusEarthMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetRadiusEarthMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetRadiusEarthMax === name) {
-          obj.planetRadiusEarthMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetRadiusEarthMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetRadiusJupiter === name) {
-          obj.planetRadiusJupiter = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetRadiusJupiter = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetRadiusJupiterMin === name) {
-          obj.planetRadiusJupiterMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetRadiusJupiterMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetRadiusJupiterMax === name) {
-          obj.planetRadiusJupiterMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetRadiusJupiterMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetMassEarth === name) {
-          obj.planetMassEarth = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetMassEarth = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetMassEarthMin === name) {
-          obj.planetMassEarthMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetMassEarthMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetMassEarthMax === name) {
-          obj.planetMassEarthMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetMassEarthMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetMassJupiter === name) {
-          obj.planetMassJupiter = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetMassJupiter = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetMassJupiterMin === name) {
-          obj.planetMassJupiterMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetMassJupiterMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetMassJupiterMax === name) {
-          obj.planetMassJupiterMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetMassJupiterMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetDensity === name) {
-          obj.planetDensity = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetDensity = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetDensityMin === name) {
-          obj.planetDensityMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetDensityMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.planetDensityMax === name) {
-          obj.planetDensityMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.planetDensityMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.equilibriumTemperature === name) {
-          obj.equilibriumTemperature = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.equilibriumTemperature = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.equilibriumTemperatureMin === name) {
-          obj.equilibriumTemperatureMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.equilibriumTemperatureMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.equilibriumTemperatureMax === name) {
-          obj.equilibriumTemperatureMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.equilibriumTemperatureMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.insolationFlux === name) {
-          obj.insolationFlux = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.insolationFlux = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.insolationFluxMin === name) {
-          obj.insolationFluxMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.insolationFluxMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.insolationFluxMax === name) {
-          obj.insolationFluxMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.insolationFluxMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarEffectiveTemperature === name) {
-          obj.stellarEffectiveTemperature = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarEffectiveTemperature = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarEffectiveTemperatureMin === name) {
-          obj.stellarEffectiveTemperatureMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarEffectiveTemperatureMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarEffectiveTemperatureMax === name) {
-          obj.stellarEffectiveTemperatureMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarEffectiveTemperatureMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarSurfaceGravity === name) {
-          obj.stellarSurfaceGravity = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarSurfaceGravity = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarSurfaceGravityMin === name) {
-          obj.stellarSurfaceGravityMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarSurfaceGravityMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarSurfaceGravityMax === name) {
-          obj.stellarSurfaceGravityMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarSurfaceGravityMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarRadius === name) {
-          obj.stellarRadius = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarRadius = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarRadiusMin === name) {
-          obj.stellarRadiusMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarRadiusMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarRadiusMax === name) {
-          obj.stellarRadiusMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarRadiusMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarMass === name) {
-          obj.stellarMass = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarMass = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarMassMin === name) {
-          obj.stellarMassMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarMassMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarMassMax === name) {
-          obj.stellarMassMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarMassMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarMetallicity === name) {
-          obj.stellarMetallicity = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarMetallicity = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarMetallicityMin === name) {
-          obj.stellarMetallicityMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarMetallicityMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarMetallicityMax === name) {
-          obj.stellarMetallicityMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarMetallicityMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarAge === name) {
-          obj.stellarAge = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarAge = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarAgeMin === name) {
-          obj.stellarAgeMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarAgeMin = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.stellarAgeMax === name) {
-          obj.stellarAgeMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.stellarAgeMax = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.rightAscension === name) {
-          obj.rightAscension = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.rightAscension = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.declination === name) {
-          obj.declination = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.declination = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.magnitude === name) {
-          obj.magnitude = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.magnitude = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.dispositionScore === name) {
-          obj.dispositionScore = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.dispositionScore = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.modelSnr === name) {
-          obj.modelSnr = values[index]?.trim() ? parseFloat(values[index]?.trim()) : undefined
+          obj.modelSnr = values[index]?.trim() ? parseFloat(values[index]?.trim()) : 0
         } else if (ExoplanetFieldEnum.tcePlanetNumber === name) {
-          obj.tcePlanetNumber = values[index]?.trim() ? parseInt(values[index]?.trim(), 10) : undefined
+          obj.tcePlanetNumber = values[index]?.trim() ? parseInt(values[index]?.trim(), 10) : 0
         }
         return obj
       },
